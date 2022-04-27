c30bfb9edd1e042d316ef205aad13309a71ff1ba
Jushraf Rahman <jushrafrahi97@gmail.com>
2022-04-28 00:37:06 +0600

    View homepage with fake dataset

--- app.js
+++ app.js
@@ -8,6 +8,38 @@ app.use(express.json());
 app.use(methodOverride('_method'));
 
 app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
+const dataset = [
+    {
+        firstName : "Jushraf",
+        lastName : "Chowdhury",
+        email : "jushrafrahi97@gmail.com",
+        phone : "12345"
+    },
+    {
+        firstName : "Mason",
+        lastName : "Mount",
+        email : "moneymase@gmail.com",
+        phone : "987"
+    },
+    {
+        firstName : "Declan",
+        lastName : "Rice",
+        email : "deccers@gmail.com",
+        phone : "41"
+    },
+    {
+        firstName : "John",
+        lastName : "Terry",
+        email : "cap@gmail.com",
+        phone : "26"
+    }
+];
+app.get('/homepage',(req,res) => {
+    console.log("At homepage!");
+    res.render('homepage',{dataset});
+})
 
 
 app.listen(3000, () => {
