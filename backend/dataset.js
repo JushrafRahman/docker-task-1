643b9f5adb9011637729e1090af15a5c75ee7742
Jushraf Rahman <jushrafrahi97@gmail.com>
2022-04-28 06:57:01 +0600

    Add fake dataset which will show during initial render

---
+++ backend/dataset.js
@@ -0,0 +1,28 @@
+const dataset = [
+    {
+        firstName : "Jushraf",
+        lastName : "Chowdhury",
+        email : "jushrafrahi97@gmail.com",
+        phone : "12345"
+    },
+    {
+        firstName : "Mason",
+        lastName : "Mount",
+        email : "moneymase@gmail.com",
+        phone : "19"
+    },
+    {
+        firstName : "Declan",
+        lastName : "Rice",
+        email : "deccers@gmail.com",
+        phone : "41"
+    },
+    {
+        firstName : "John",
+        lastName : "Terry",
+        email : "cap@gmail.com",
+        phone : "26"
+    }
+];
+
+module.exports = dataset;
